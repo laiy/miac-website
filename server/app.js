d34b83f761d479be2b996d53cfc82bbca502265f
laiy <ly.franky@gmail.com>
2014-08-23 15:39:45 +0800

    Update indents

--- server/app.js
+++ server/app.js
@@ -27,56 +27,56 @@ app.use(express.static(path.join(__dirname, 'public')));
 var db = require('./lib/db');
 db.connect(function(db) {
 
-  app.use('/admin', cookieParser(config.cookieSecret));
-  app.use('/admin', session());
+    app.use('/admin', cookieParser(config.cookieSecret));
+    app.use('/admin', session());
 
-  // routes
-  app.use('/admin', require('./routes/admin'));
-  app.use('/api', require('./routes/api'));
-  app.use('/', require('./routes/test'));
+    // routes
+    app.use('/admin', require('./routes/admin'));
+    app.use('/api', require('./routes/api'));
+    app.use('/', require('./routes/test'));
 
-  // Test page
-  if (app.get('env') === 'development') {
-    app.get('/test', function(req, res){
-      res.render('test');
+    // Test page
+    if (app.get('env') === 'development') {
+        app.get('/test', function(req, res){
+            res.render('test');
+        });
+    }
+
+    /// catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
     });
-  }
 
-  /// catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+    /// error handlers
 
-  /// error handlers
+    // development error handler
+    // will print stacktrace
+    if (app.get('env') === 'development') {
+        app.use(function(err, req, res, next) {
+            res.status(err.status || 500);
+            console.log(err);
+            // res.render('error', {
+            //   message: err.message,
+            //   error: err
+            // });
+        });
+    }
 
-  // development error handler
-  // will print stacktrace
-  if (app.get('env') === 'development') {
+    // production error handler
+    // no stacktraces leaked to user
     app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      console.log(err);
-      // res.render('error', {
-      //   message: err.message,
-      //   error: err
-      // });
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
     });
-  }
 
-  // production error handler
-  // no stacktraces leaked to user
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: {}
+    var server = app.listen(process.env.PORT || 3000, function() {
+        console.log('Express server listening on port ' + server.address().port);
     });
-  });
-
-  var server = app.listen(process.env.PORT || 3000, function() {
-    console.log('Express server listening on port ' + server.address().port);
-  });
 
 });
 
