2ad55707204a6510fa00a1a34a223d5885a56d54
laiy <ly.franky@gmail.com>
2014-08-24 10:32:35 +0800

    Update division of work

--- server/routes/test.js
+++ server/routes/test.js
@@ -3,11 +3,87 @@ var router = express.Router();
 var album = require('../test/album');
 
 router.get('/', function(req, res, next) {
-    res.render('test', {test: "test"});
+    res.render('index');
+});
+
+router.get('/announcement', function(req, res, next) {
+    res.render('announcement');
+});
+
+router.get('/article', function(req, res, next) {
+    res.render('article');
+});
+
+router.get('/article/id', function(req, res, next) {
+    res.render('childArticle');
+});
+
+router.get('/article/create', function(req, res, next) {
+    res.render('createArticle');
+});
+
+router.get('/share', function(req, res, next) {
+    res.render('share');
+});
+
+router.get('/share/id', function(req, res, next) {
+    res.render('childShare');
+});
+
+router.get('/share/create', function(req, res, next) {
+    res.render('createShare');
+});
+
+router.get('/discuss', function(req, res, next) {
+    res.render('discuss');
+});
+
+router.get('/discuss/id', function(req, res, next) {
+    res.render('childDiscuss');
+});
+
+router.get('/discuss/create', function(req, res, next) {
+    res.render('createDiscuss'); 
 });
 
 router.get('/album', function(req, res, next) {
+    res.render('album');
+});
+
+router.get('/album/id', function(req, res, next) {
+    res.render('childAlbum'); 
+});
+
+router.get('/album/create', function(req, res, next) {
+    res.render('createAlbum');
+});
+
+router.get('/works', function(req, res, next) {
+    res.render('works');  
+});
+
+router.get('/works/create', function(req, res, next) {
+    res.render('createWorks');
+});
+
+router.get('/about', function(req, res, next) {
+    res.render('about');
+});
+
+router.get('/logIn', function(req, res, next) {
+    res.render('logIn');
+});
+
+router.get('/register', function(req, res, next) {
+    res.render('register'); 
+});
+
+router.get('/albumTest', function(req, res, next) {
     res.render('albumTest', {album: album});
+});
+
+router.get('/test', function(req, res, next) {
+    res.render('test');
 });
 
 module.exports = router;
