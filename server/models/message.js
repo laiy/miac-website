33eb12ffcf49f57b1b9064ad419f62d79d96309f
laiy <ly.franky@gmail.com>
2014-08-26 16:07:22 +0800

    Finish message model originally

--- server/models/message.js
+++ server/models/message.js
@@ -1,3 +1,63 @@
-/*
- *todo
- */
+var async = require('async');
+var db = require('../lib/db').getConnection();
+
+module.exports = {
+
+    findOne: function(selector, callback) {
+        async.waterfall([
+            function(callback) {
+                db.connection('message', callback);
+            },
+            function(col, callback) {
+                col.findOne(selector, callback);
+            }
+        ], callback);
+    },
+
+    find: function(selector, options, callback) {
+        async.waterfall([
+            function(callback) {
+                db.connection('message', callback);
+            },
+            function(col, callback) {
+                col.find(selector, options).toArray(callback);
+            }
+        ], callback);
+    },
+
+    insert: function(message, callback) {
+        async.waterfall([
+            function(callback) {
+                db.connection('message', callback);
+            },
+            function(col, callback) {
+                db.insert(message, callback);
+            }
+        ], function(err, items) {
+            callback(err, items && items[0]);
+        });
+    },
+
+    update: function(selector, updater, callback) {
+        async([
+            function(callback) {
+                db.connection('message', callback);
+            },
+            function(col, callback) {
+                col.update(selector, updater, callback);
+            } 
+        ], callback);
+    },
+
+    remove: function(selector, callback) {
+        async.waterfall([
+            function(callback) {
+                db.connection('message', callback);
+            },
+            function(col, callback) {
+                col.remove(selector, callback);
+            }
+        ], callback);
+    }
+
+};
