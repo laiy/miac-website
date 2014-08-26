585351ac53a99400e476bd1b8ea3f45fd9224ba1
laiy <ly.franky@gmail.com>
2014-08-26 10:37:08 +0800

    Update user model

--- server/models/user.js
+++ server/models/user.js
@@ -46,7 +46,105 @@ module.exports = {
                 col.find(selector, options).toArray(callback);
             }
         ], callback);
+    },
+
+    insert: function(user, callback) {
+        async.waterfall([
+            function(callback) {
+                db.connection('user', callback);
+            },
+            function(col, callback) {
+                db.insert(user, callback);
+            }
+        ], function(err, items) {
+            callback(err, items && items[0]);
+        });
+    },
+
+    update: function(selector, updater, callback) {
+        async([
+            function(callback) {
+                db.connection('user', callback);
+            },
+            function(col, callback) {
+                col.update(selector, updater, callback);
+            } 
+        ], callback);
+    },
+
+    remove: function(selector, callback) {
+        async.waterfall([
+            function(callback) {
+                db.connection('user', callback);
+            },
+            function(col, callback) {
+                col.remove(selector, callback);
+            }
+        ], callback);
+    },
+
+    signIn: function(username, password, callback) {
+        var User = this;
+        var user;
+        async.waterfall([
+            function(callback) {
+                User.findOne({username: username}, callback);
+            },
+            function(item, callback) {
+                if (!item || encryptPassword(password, item.password.salt) != item.password.identity) {
+                    callback(true);
+                } else {
+                    user = item;
+                    createSession(item._id, callback);
+                }
+            }
+        ], function(err, sid) {
+            callback(err, user, sid);
+        });
+    },
+
+    signUp: function(doc, callback) {
+        var User = this;
+        var user;
+        async.waterfall([
+            function(callback) {
+                User.insert(doc, callback);
+            },
+            function(item, callback) {
+                if (!item) {
+                    callback(true);
+                } else {
+                    user = item;
+                    createSession(item._id, callback);
+                }
+            }
+        ], function(err, sid) {
+            callback(err, user, sid);
+        });
+    },
+
+    checkPassword: function(userId, password, callback) {
+        this.findOne({_id: userId}, function(err, item) {
+            callback(err, item && encryptPassword(password, item.password.salt) == item.password.identity);
+        });
+    },
+
+    validateUsername: function(username) {
+        if (!username) {
+            return 'Username should not be empty';
+        } else if (!/^[a-zA-Z\d]+$/.test(username)) {
+            return 'The form of username is invalid';
+        }
+        return false;
+    },
+
+    validatePassword: function(password) {
+        if (!password || password.length < 6) {
+            return 'The length of password should not be less than 6';
+        }
+        return false;
     }
+    
 
 };
 
