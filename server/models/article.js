d783a14617ac996a8ea19cc8b0daa446394c649d
laiy <ly.franky@gmail.com>
2014-08-27 17:24:41 +0800

    Add article modewl

--- server/models/article.js
+++ server/models/article.js
@@ -1,3 +1,63 @@
-/*
- *todo
- */
+var async = require('async');
+var db = require('../lib/db').getConnection();
+
+module.exports = {
+
+    findOne: function(selector, callback) {
+        async.waterfall([
+            function(next) {
+                db.connection('article', next);
+            },
+            function(col, next) {
+                col.findOne(selector, next);
+            }
+        ], callback);
+    },
+
+    find: function(selector, options, callback) {
+        async.waterfall([
+            function(next) {
+                db.connection('article', next);
+            },
+            function(col, next) {
+                col.find(selector, options).toArray(next);
+            }
+        ], callback);
+    },
+
+    insert: function(article, callback) {
+        async.waterfall([
+            function(next) {
+                db.connection('article', next);
+            },
+            function(col, next) {
+                db.insert(article, next);
+            }
+        ], function(err, items) {
+            callback(err, items && items[0]);
+        });
+    },
+
+    update: function(selector, updater, callback) {
+        async([
+            function(next) {
+                db.connection('article', next);
+            },
+            function(col, next) {
+                col.update(selector, updater, next);
+            } 
+        ], callback);
+    },
+
+    remove: function(selector, callback) {
+        async.waterfall([
+            function(next) {
+                db.connection('article', next);
+            },
+            function(col, next) {
+                col.remove(selector, next);
+            }
+        ], callback);
+    }
+
+};
