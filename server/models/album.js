1149ec3c0e5d5fa64fad945d62badf6e3523eef7
laiy <ly.franky@gmail.com>
2014-08-27 17:03:13 +0800

    Add album model

--- server/models/album.js
+++ server/models/album.js
@@ -1,3 +1,63 @@
-/*
- *todo
- */
+var async = require('async');
+var db = require('../lib/db').getConnection();
+
+module.exports = {
+
+    findOne: function(selector, callback) {
+        async.waterfall([
+            function(next) {
+                db.connection('album', next);
+            },
+            function(col, next) {
+                col.findOne(selector, next);
+            }
+        ], callback);
+    },
+
+    find: function(selector, options, callback) {
+        async.waterfall([
+            function(next) {
+                db.connection('album', next);
+            },
+            function(col, next) {
+                col.find(selector, options).toArray(next);
+            }
+        ], callback);
+    },
+
+    insert: function(album, callback) {
+        async.waterfall([
+            function(next) {
+                db.connection('album', next);
+            },
+            function(col, next) {
+                db.insert(album, next);
+            }
+        ], function(err, items) {
+            callback(err, items && items[0]);
+        });
+    },
+
+    update: function(selector, updater, callback) {
+        async([
+            function(next) {
+                db.connection('album', next);
+            },
+            function(col, next) {
+                col.update(selector, updater, next);
+            } 
+        ], callback);
+    },
+
+    remove: function(selector, callback) {
+        async.waterfall([
+            function(next) {
+                db.connection('album', next);
+            },
+            function(col, next) {
+                col.remove(selector, next);
+            }
+        ], callback);
+    }
+
+};
