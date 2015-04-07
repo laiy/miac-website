7d672d5e0528418ed749ec36be55d00b52545091
williamking <williamjwking@gmail.com>
2015-04-07 17:22:31 +0800

    ..

--- server/views/bin/js/createArticle.js
+++ server/views/bin/js/createArticle.js
@@ -1,18 +1,20 @@
 (function() {
-  $('button').click(function() {
-    var category, content, title;
-    category = $('#category').val();
-    title = $('#title').val();
-    content = $('#content').val();
-    return $.post('/article/create', {
-      category: category,
-      title: title,
-      content: content
-    }, function(data) {
-      alert(data.result + '\n' + (data.msg ? data.msg : void 0));
-      if (data.result === 'success') {
-        return $(location).attr('href', '/article');
-      }
+  $(function() {
+    return $('.submit').click(function() {
+      var category, content, title;
+      category = $('#category').val();
+      title = $('#title').val();
+      content = $('#content').val();
+      return $.post('/article/create', {
+        category: category,
+        title: title,
+        content: content
+      }, function(data) {
+        alert(data.result + '\n' + (data.msg ? data.msg : void 0));
+        if (data.result === 'success') {
+          return $(location).attr('href', '/article');
+        }
+      });
     });
   });
 
