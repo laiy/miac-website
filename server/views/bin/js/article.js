0991b73347ed2ae05be1a0b1e3a4520ea770eb86
williamking <williamjwking@gmail.com>
2015-09-18 21:12:58 +0800

    page-devide

--- server/views/bin/js/article.js
+++ server/views/bin/js/article.js
@@ -18,4 +18,32 @@ $(function() {
     	$(this).empty();
     	$(this).append(cutstr(sTest.stripHTML()));
     });
+    
+    //Set the link of page button
+    var length = $(".page-button").length,
+        tag = url('?tag'),
+        page = url('?page');
+    if (tag == null) tag = '';
+    if (page == null) page = "1";
+    page = parseInt(page);
+    if (page > 1) {
+        $('#prev-page').find('a').attr("href", "/Article?page=" + (page - 1) + "&tag=" + tag);
+    } else {
+        $('#prev-page').find('a').attr("href", "#");
+    }
+    if (page < length) {
+        $('#next-page').find('a').attr("href", "/Article?page=" + (page + 1) + "&tag=" + tag);
+    } else {
+        $('#next-page').find('a').attr("href", "#");
+    }
+    $(".page-button").each(function() {
+        var num = $(this).find('a').text();
+        $(this).find('a').attr("href", "/Article?page=" + num + "&tag=" + tag);
+        if (parseInt(num) == page) {
+            $(this).addClass('choosen-page');
+        } else {
+            $(this).removeClass('choosen-page');
+        }
+    });
+
 });
