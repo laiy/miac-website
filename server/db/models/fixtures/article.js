65bbf9e0a7ccdae150be3bfcd1b5f57672e1b338
laiy <ly.franky@gmail.com>
2015-09-20 09:53:57 +0800

    removed unused fixtures

--- server/db/models/fixtures/article.js
+++
@@ -1,8 +0,0 @@
-var article = {
-  _id: "article-id-1", // ObjectID
-  title: "The new ODM framework of document database!", // string
-  content: "", // string
-  createdBy:  "user-id-1", // ObjectID
-  createdAt: "2014-8-21" // Date
-};
-
