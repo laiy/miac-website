040961122aa295b42e31a76f59f663a4086a3967
laiy <ly.franky@gmail.com>
2015-09-27 11:03:36 +0800

    Test commit. Please ignore

--- server/views/bin/js/message.js
+++ server/views/bin/js/message.js
@@ -3,19 +3,19 @@ messageCallback = function() {
 
 (function() {
     $("#message-confirm").click(function() {
-       messageFadeOut(); 
+       messageFadeOut();
     });
 
     messageFadeIn = function(mess) {
         $("#message-content").find("p").text(mess);
         $("#message").removeClass("invisible").addClass("extent-in-animate").removeClass("extent-out-animate");
-    }
+    };
     messageFadeOut = function() {
         $("#message").removeClass("extent-in-animate").addClass("extent-out-animate").addClass("invisible");
-    }
+    };
 
     messageCallback = function() {
-    }
+    };
 
     $("#message-confirm").click(messageCallback);
 
@@ -23,13 +23,13 @@ messageCallback = function() {
         e.preventDefault();
         $("#login-modal").removeClass("invisible").addClass("extent-in-animate").removeClass("extent-out-animate");
         $("#cover").removeClass("invisible");
-    }); 
+    });
 
     $("#close-btn").click(function(e) {
         e.preventDefault();
         $("#cover").addClass("invisible");
         $("#login-modal").removeClass("extent-in-animate").addClass("extent-out-animate").addClass("invisible");
-    }); 
+    });
 
     $("#message-confirm").click(messageCallback);
 
